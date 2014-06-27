3c9200b54717654fc8d274bd3080cba661372a95
ilackarms <sdw35@cornell.edu>
2014-06-26 23:25:44 -0400

    Added Global class GameData to store music, possibly otehr things later on (like character selection, volume, other menu choices) Just started CharacterSelectScreen, haven't done anything in it yet. Got the clickListener working for buttons. Buttons that exist (but don't fuinction yet) on MainMenuScreen: exit, 1 player, 2 player  need to figure out how to space buttons, put backgrounds on stages (if that's the right way to do it)

---
+++ core/src/com/battlefighters/global/GameData.java
@@ -0,0 +1,11 @@
+package com.battlefighters.global;
+
+import com.badlogic.gdx.audio.Music;
+
+/**
+ * Created by Dell_Owner on 6/26/2014.
+ */
+public final class GameData {
+    public static Music music;
+    public static float VOLUME = 0.05f;
+}
---
+++ core/src/com/battlefighters/screens/CharacterSelectScreen.java
@@ -0,0 +1,7 @@
+package com.battlefighters.screens;
+
+/**
+ * Created by Dell_Owner on 6/26/2014.
+ */
+public class CharacterSelectScreen {
+}
